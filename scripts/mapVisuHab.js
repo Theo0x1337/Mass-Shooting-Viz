ec0da885294c014f432acd8d84c2838b5b4eac35
Theo Bernardin <theo.bernardin@gmail.com>
2020-11-25 12:12:39 +0100

    oui

--- scripts/mapVisuHab.js
+++ scripts/mapVisuHab.js
@@ -15,38 +15,38 @@ var width = 960;
 var height = 500;
 
 // D3 Projection
-var projection6 = d3.geo.albersUsa()
+var projection = d3.geo.albersUsa()
 				   .translate([width/2, height/2])    // translate to center of screen
 				   .scale([1000]);          // scale things down so see entire US
         
 // Define path generator
-var path6 = d3.geo.path()               // path generator that will convert GeoJSON to SVG paths
-		  	 .projection(projection6);  // tell path generator to use albersUsa projection
+var path = d3.geo.path()               // path generator that will convert GeoJSON to SVG paths
+		  	 .projection(projection);  // tell path generator to use albersUsa projection
 
 		
 // Define linear scale for output
 var color6 = d3.scale.linear()
 				.range(["rgb(213,222,217)","rgb(69,173,168)","rgb(217,91,67)","rgb(84,36,55)"]);
 
-var legendText6 = ["Fatalities >= 50", "50 > Fatalities >= 25", " 25 > Fatalities >= 1", "No fatalities"];
+var legendText6 = ["Inhabitants < 1 000 000 ", "Inhabitants < 5 000 000", " Inhabitants < 20 000 000", "Inhabitants < 50 000 000"];
 
 //Create SVG element and append map to the SVG
 var svg6 = d3.select("#mapUsHab")
-			.append("div").attr("class", "map")
+			.append("div").attr("class", "mapUsHab")
 			.append("svg")
 			.attr("width", width)
 			.attr("height", height);
         
 // Append Div for tooltip to SVG
-var div6 = d3.select("#mapUs")
+var div = d3.select("#mapUsHab")
 		    .append("div")   
-    		.attr("class", "tooltip")               
+    		.attr("class", "tooltip2")               
             .style("opacity", 0);
             
 
 // Load in my states data!
 d3.csv("habitants.csv", function(data) {
-color6.domain([0,1,2,3]); // setting the range of the input data
+color6.domain([1000000,5000000,2000000,50000000]); // setting the range of the input data
 
 // Load GeoJSON data and merge with states data
 d3.json("us-states.json", function(json) {
@@ -58,13 +58,13 @@ for (var i = 0; i < data.length; i++) {
 	var dataState = data[i].states;
 
 	// Grab data value 
-	var dataValue = data[i].values;
-
-	console.log(dataValue)
+    var dataValue = data[i].values;
 
 	// Find the corresponding state inside the GeoJSON
 	for (var j = 0; j < json.features.length; j++)  {
 		var jsonState = json.features[j].properties.name;
+
+        console.log(dataState == jsonState)
 
 		if (dataState == jsonState) {
 
@@ -76,32 +76,34 @@ for (var i = 0; i < data.length; i++) {
 		}
 	}
 }
+
+console.log(json.features)
 		
 // Bind the data to the SVG and create one path per GeoJSON feature
 svg6.selectAll("path")
 	.data(json.features)
 	.enter()
 	.append("path")
-	.attr("d", path6)
+	.attr("d", path)
 	.style("stroke", "#fff")
 	.style("stroke-width", "1")
 	.style("fill", function(d) {
 
 	// Get data value
-	var value6 = d.properties.values;
+	var value = d.properties.values;
 
-	if (value6) {
+	if (value) {
 	//If value exists…
-	return color(value6);
+	return color6(value);
 	} else {
 	//If value is undefined…
 	return "rgb(213,222,217)";
 	}
-}); 
-        
+});
+         
 // Modified Legend Code from Mike Bostock: http://bl.ocks.org/mbostock/3888852
-var legend2 = d3.select("#mapUsHab").append("svg")
-      			.attr("class", "legend")
+var legend = d3.select("#mapUsHab").append("svg")
+      			.attr("class", "legend2")
      			.attr("width", 180)
     			.attr("height", 200)
    				.selectAll("g")
@@ -110,13 +112,13 @@ var legend2 = d3.select("#mapUsHab").append("svg")
    				.append("g")
      			.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
-  	legend2.append("rect")
+  	legend.append("rect")
    		  .attr("width", 18)
    		  .attr("height", 18)
    		  .style("fill", color6);
 
-  	legend2.append("text")
-  		  .data(legendText6)
+  	legend.append("text")
+  		  .data(legendText)
       	  .attr("x", 24)
       	  .attr("y", 9)
       	  .attr("dy", ".35em")
